3fa499774d176d6873f3c9eefc5b25337caa347f
Christopher Ferris <cferris@google.com>
2014-01-31 15:07:26 -0800

    Allow 64 bit host library to build.
    Change-Id: I0911e11d07e314e2ef5ffda187ea345d748c2118

--- Android.mk
+++ Android.mk
@@ -20,6 +20,9 @@ build_host := false
 ifeq ($(HOST_OS),linux)
 ifeq ($(HOST_ARCH),$(filter $(HOST_ARCH),x86 x86_64))
 build_host := true
+ifneq ($(strip $(BUILD_HOST_64bit)),)
+HOST_ARCH := x86_64
+endif
 endif
 endif
 
